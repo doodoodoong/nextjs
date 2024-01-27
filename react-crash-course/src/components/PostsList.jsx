7ea956da50f1798bf47d63f27f3df58516f24d69
doodoodoong <doo_doong@hanmail.net>
2024-01-27 22:44:13 +0900

    useState

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,11 +1,15 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Kim" body="React.js is awesome!" />
-      <Post author="Yi" body="Check out the full course" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Kim" body="React.js is awesome!" />
+        <Post author="Yi" body="Check out the full course" />
+      </ul>
+    </>
   );
 }
